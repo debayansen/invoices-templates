2a74e1a82561e392b20e5460b9fb8666867e4c50
debayansen <debayan.s@emamifrankross.com>
2017-05-03 12:42:34 +0530

    updated Address css

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,10 @@ gulp.task('dev-styles', function(){
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+        // .pipe(autoprefixer({
+        //     browsers: ['last 2 versions'],
+        //     cascade: false
+        // }))
         .pipe(gulp.dest('css/'))
         .pipe(browsersync.reload({stream:true}));
     console.log("styles finished");
